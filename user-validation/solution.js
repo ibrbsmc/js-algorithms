5818ad33d52fddb4e2aa96e021dee750f5396a6a
İbrahim Basmacı <ibrbsmc@gmail.com>
2026-07-22 23:00:42 +0300

    Add user-validation solution

---
+++ user-validation/solution.js
@@ -0,0 +1,29 @@
+/* Rules
+
+1. The username is between 4 and 25 characters.
+2. It must start with a letter.
+3. It can only contain letters, numbers, and the underscore character.
+4. It cannot end with an underscore character.
+
+If the username is valid then your program should return the string true,
+otherwise return the string false.
+
+u__hello_world123
+
+*/
+
+function validateUsr(username) {
+  if (username.length >= 4 && username.length <= 25) {
+    if (/^\p{L}/u.test(username)) {
+      if (/^[\p{L}0-9_]+$/u.test(username)) {
+        if (!/_$/.test(username)) {
+          return true;
+        }
+      }
+    }
+  }
+
+  return false;
+}
+
+console.log(validateUsr("İbrahim"));
